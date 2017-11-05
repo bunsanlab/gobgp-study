585b6e6ebdb5d81735255765d515131fa96ae5b1
yatuhashi kei <yatuhashi@alice-tech.net>
2017-11-05 16:21:29 +0900

    masyumotokyu-n

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 $(docker-compose up -d)
 $(docker-compose stop)
 $(docker network connect --ip 10.0.1.2 gobgpstudy_LAN2 C2)
+$(docker network connect --ip 10.0.2.3 gobgpstudy_LAN3 C3)
 $(docker-compose start)
 
 $(docker-compose exec C1 /bin/bash /etc/init.d/quagga restart)
