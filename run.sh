0848b2ba64d4ed44643b77b330a1e51434f048be
yatuhashi kei <yatuhashi@alice-tech.net>
2017-11-05 15:29:55 +0900

    mpls-masumoto

--- run.sh
+++ run.sh
@@ -2,3 +2,7 @@ $(docker-compose up -d)
 $(docker-compose stop)
 $(docker network connect --ip 10.0.1.2 gobgpstudy_LAN2 C2)
 $(docker-compose start)
+
+$(docker-compose exec C1 /bin/bash /etc/init.d/quagga restart)
+$(docker-compose exec C2 /bin/bash /etc/init.d/quagga restart)
+$(docker-compose exec C3 /bin/bash /etc/init.d/quagga restart)
